9076ba805f11b7f671985a318f5ae0689ed7b382
William Tran <wtran29@gmail.com>
2023-03-05 22:10:28 -0800

    updated the solidity contract to have the ability to cast votes -voters could not vote twice and candidate must be valid, generated abi, bin and binding go files to migrate updated contract to ganache for testing

--- app/contracts/src/Ballot.sol
+++ app/contracts/src/Ballot.sol
@@ -9,6 +9,8 @@ contract Ballot {
         string name;
         uint voteCount;
     }
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
     // Store Candidates
     // Fetch Candidate
     mapping(uint => Candidate) public candidates;
@@ -23,5 +25,17 @@ contract Ballot {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        // record that voter has voted
+        voters[msg.sender] = true;
+        // Update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+        
     }
 }
